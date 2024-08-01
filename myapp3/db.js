24608b3cc26c2736aa54a79ea6abf0d4d0d9a544
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-08-01 16:31:34 +0530

    database connection

---
+++ myapp3/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const url = "mongodb://127.0.0.1:27017/hotels";
+
+mongoose.connect(url);
+
+const db = mongoose.connection;
+
+db.on("connected", () => {
+  console.log("Mongoose connected to database");
+});
+
+db.on("error", (err) => {
+  console.log("Mongoose error: " + err);
+});
+db.on("disconnected", () => {
+  console.log("MongoDB disconnected");
+});
+
+module.exports = db;
