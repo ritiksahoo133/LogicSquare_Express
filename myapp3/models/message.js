94f0e48a6bb8f27e55c429c6d90bec2472f128d3
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-08-06 17:35:28 +0530

    add _conversation in messageSchema

--- myapp3/models/message.js
+++ myapp3/models/message.js
@@ -9,6 +9,12 @@ const messageSchema = new mongoose.Schema({
 
   _to: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 
+  _conversation: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Conversation",
+    required: true,
+  },
+
   text: { type: String, required: true, trim: true },
 
   date: { type: Date, required: true },
