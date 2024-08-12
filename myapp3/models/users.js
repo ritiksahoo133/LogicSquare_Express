f60da3a62f02efd83bebe62e75881279ec4e077f
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-08-12 12:39:30 +0530

    hash password and compare password

--- myapp3/models/users.js
+++ myapp3/models/users.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = new mongoose.Schema(
   {
@@ -11,13 +13,35 @@ const userSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
-    password: { type: String, required: true, trim: true },
-    lastLoggedInDate: { type: Date, default: Date.now() },
+    password: { type: String, trim: true },
+    lastLoggedInDate: { type: Date, default: null },
     backgroundColor: { type: String, default: "#ffffff" },
     imageUrl: { type: String },
   },
   { timestamps: true }
 );
+
+//hash & save user's password
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (this.isModified("password") || this.isNew) {
+    try {
+      user.password = await bcrypt.hash(user.password, 10);
+    } catch (error) {
+      return next(error);
+    }
+  }
+  return next();
+});
+
+userSchema.methods.comparePassword = async function (pw) {
+  try {
+    const isMatch = await bcrypt.compare(pw, this.password);
+    if (!isMatch) throw new Error("Invalid username or password");
+  } catch (error) {
+    throw error;
+  }
+};
 
 const User = mongoose.model("User", userSchema);
 
