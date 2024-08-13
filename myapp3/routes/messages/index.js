54a6440ae889fa88b0526455d88e35c1fa6c936e
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-08-13 15:32:15 +0530

    create all message routers

---
+++ myapp3/routes/messages/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const authJwtMiddleware = require("../../middleware/authmiddlewire");
+
+const message = require("../messages/chat");
+
+router.post("/sendmessage", authJwtMiddleware, message.sendmessage);
+router.get("/getmessage", authJwtMiddleware, message.getmessage);
+router.delete("/:id", authJwtMiddleware, message.deletemessage);
+
+module.exports = router;
