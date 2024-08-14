380b92788290b62ff4d4e9b20220040591429cce
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-08-14 17:55:05 +0530

    create router location

--- myapp3/routes/messages/index.js
+++ myapp3/routes/messages/index.js
@@ -7,6 +7,14 @@ const message = require("../messages/chat");
 router.post("/sendmessage", authJwtMiddleware, message.sendmessage);
 router.get("/getmessage", authJwtMiddleware, message.getmessage);
 router.delete("/:id", authJwtMiddleware, message.deletemessage);
-router.get("/getconversation/:id",message.getConversation)
+router.get("/getconversation/:id", message.getConversation);
+
+router.get("/notcontaingmail", message.notcontaingmail);
+
+//add location
+router.post("/add", message.addlocation);
+
+//near
+router.get("/near", message.near);
 
 module.exports = router;
