d44f551f4ef49fc312c6c375a7a651895f7bdbd9
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-08-20 15:11:17 +0530

    schedule method

--- myapp3/agenda/index.js
+++ myapp3/agenda/index.js
@@ -15,10 +15,21 @@ agenda.define("welcomeMessage", () => {
 agenda.define("Hello", () => {
   console.log("Hello world");
 });
+agenda.define("sendemail", async (job) => {
+  const { to, subject, body } = job.attrs.data;
+  console.log(`Sending email to ${to} with subject ${subject}`);
+});
 
 agenda.on("ready", async () => {
-  await agenda.every("3 seconds", "Hello"); // scheduling jobs
+  //   await agenda.every("3 seconds", "Hello"); // scheduling jobs
   await agenda.start(); // start execution
   console.log("Agenda started");
+
+  // schedule method
+  await agenda.schedule("4 seconds", "sendemail", {
+    to: "user123@gmail.com",
+    subject: "Meeting reminder",
+  });
+  console.log(`Email scheduled to run at 4 second`);
 });
 module.exports = agenda;
