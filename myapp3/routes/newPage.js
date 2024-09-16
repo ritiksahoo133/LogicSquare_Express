558a0a73fb1c955eb47769b8c7ca395a49158b08
Ritik Kumar Sahoo <ritiksahoo133@gmail.com>
2024-09-16 11:54:49 +0530

    initial commit

--- myapp3/routes/newPage.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  console.log("Hello");
-  res.render("newPage");
-});
-
-module.exports = router;
